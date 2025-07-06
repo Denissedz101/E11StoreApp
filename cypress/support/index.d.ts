1c55d48cc9c16e4fb6af2a0564e60abdab1f679a
Denisse Díaz <denissedz@gmail.com>
2025-07-06 13:33:42 -0400

    eliminar archivos sin uso

--- cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    /**
-     * Login como un usuario específico usando correo y contraseña
-     * @example cy.loginAs('admin@admin.cl', '1234')
-     */
-    loginAs(email: string, password: string): Chainable<void>;
-  }
-}
